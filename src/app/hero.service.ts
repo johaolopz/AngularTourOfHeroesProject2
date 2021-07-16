47ef4a9ef38907b73eb1faafaa5b4716400773f3
Johao Lopez <johao.lopz.m@gmail.com>
2021-07-15 19:46:28 -0500

    Creación MessageComponent, inyectarlo y enviarlo desde HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 //simulación de obtención de datos del servidor con RxJS
 import{Observable, of} from 'rxjs';
@@ -12,9 +13,13 @@ export class HeroService {
   
   //of(HEROES) devuelve una Observable<Hero[]>
   getHeroes():Observable<Hero[]> {
+
+    // TODO: Envía el mensaje luego del fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+
     return of (HEROES);
   }
 
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
